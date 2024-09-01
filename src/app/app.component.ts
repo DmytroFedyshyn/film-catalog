957d0ef0826e8577ff456e3211bd87ae64fc3d2b
DmytroFedyshyn <93495794+DmytroFedyshyn@users.noreply.github.com>
2024-09-01 23:24:17 +0300

    add assets folder and change app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,59 @@
-import {Component} from '@angular/core';
-import {Film} from "../models/film";
-import {films} from "./stub/filmsData"
-import {genreList} from "./stub/genreList";
-import {ModalService} from "./services/modal.service";
-import {emptyFilm} from "./stub/images/emptyFilm";
-import {BehaviorSubject} from "rxjs";
-import {MatSelectChange} from "@angular/material/select";
+import { Component, ChangeDetectorRef, AfterViewInit } from '@angular/core';
+import { Film } from "../models/film";
+import { films } from "./stub/filmsData";
+import { genreList } from "./stub/genreList";
+import { ModalService } from "./services/modal.service";
+import { emptyFilm } from "./stub/images/emptyFilm";
+import { BehaviorSubject } from "rxjs";
+import { MatSelectChange } from "@angular/material/select";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  resetSearch = 'resetSearch'
-  title = 'Film Catalog'
-  filmName = ''
-  selectedGenreId = ''
-  films: Film[] = films
-  sortedFilms: Film[] = films
-  genreList = genreList
-  isLikedFilm$ = new BehaviorSubject<boolean>(true)
+export class AppComponent implements AfterViewInit {
+  resetSearch = 'resetSearch';
+  title = 'Film Catalog';
+  filmName = '';
+  selectedGenreId = '';
+  films: Film[] = films;
+  sortedFilms: Film[] = films;
+  genreList = genreList;
+  isLikedFilm$ = new BehaviorSubject<boolean>(true);
+
+  constructor(public modalService: ModalService, private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
 
   getSelectedGenreID = (genreName: string) => {
-    const genreFromList = genreList.find(el => el.genre === genreName)
-    return genreFromList?.id
+    const genreFromList = genreList.find(el => el.genre === genreName);
+    return genreFromList?.id;
   }
 
   getLikedFilm = () => {
-    const likedFilm = localStorage.getItem('likedFilm')
+    const likedFilm = localStorage.getItem('likedFilm');
     if (!likedFilm) {
-      this.isLikedFilm$.next(false)
-      return emptyFilm
+      this.isLikedFilm$.next(false);
+      return emptyFilm;
     }
-    const chosenFilm = films.find(film => film.id.toString() === likedFilm)
+    const chosenFilm = films.find(film => film.id.toString() === likedFilm);
     if (!chosenFilm) {
-      this.isLikedFilm$.next(false)
-      return emptyFilm
+      this.isLikedFilm$.next(false);
+      return emptyFilm;
     } else {
-      this.isLikedFilm$.next(true)
-      return chosenFilm
+      this.isLikedFilm$.next(true);
+      return chosenFilm;
     }
   }
 
   onSelect = ($event: MatSelectChange) => {
     if ($event.value === this.resetSearch) {
-      this.sortedFilms = this.films
-      return
+      this.sortedFilms = this.films;
+      return;
     }
-    this.sortedFilms = this.films.filter(film => film.genre.find(genre => genre.toString() === $event.value))
-  }
-
-  constructor(public modalService: ModalService) {
+    this.sortedFilms = this.films.filter(film => film.genre.find(genre => genre.toString() === $event.value));
   }
 }
-
