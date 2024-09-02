d937e511e3e1ac3ccc287bbca42db8a03742b22e
DmytroFedyshyn <93495794+DmytroFedyshyn@users.noreply.github.com>
2024-09-02 13:08:07 +0300

    remove comment

--- src/app/stub/actorsList.ts
+++ src/app/stub/actorsList.ts
@@ -1,18 +1,18 @@
 export const actorsList = [
-    { id: '1', actor: 'Liam Neeson' },            // Schindler's List
-    { id: '2', actor: 'Viggo Mortensen' },        // The Lord of the Rings: The Two Towers
-    { id: '3', actor: 'Russell Crowe' },          // Gladiator
-    { id: '4', actor: 'Mel Gibson' },             // Hacksaw Ridge
-    { id: '5', actor: 'Leonardo DiCaprio' },      // Shutter Island
-    { id: '6', actor: 'Daniel Radcliffe' },       // Harry Potter and the Deathly Hallows: Part II
-    { id: '7', actor: 'Tobin Bell' },             // Saw III
-    { id: '8', actor: 'Michael J. Fox' },         // Back to the Future Part II
-    { id: '9', actor: 'Ewan McGregor' },          // Star Wars: Episode III – Revenge of the Sith
-    { id: '10', actor: 'Hugh Jackman' },          // The Prestige
-    { id: '11', actor: 'Emma Watson' },           // Harry Potter and the Deathly Hallows: Part II
-    { id: '12', actor: 'Orlando Bloom' },         // The Lord of the Rings: The Two Towers
-    { id: '13', actor: 'Johnny Depp' },           // Pirates of the Caribbean (не в списку, додано для варіативності)
-    { id: '14', actor: 'Mark Hamill' },           // Star Wars: Episode III – Revenge of the Sith
-    { id: '15', actor: 'Scarlett Johansson' }     // Avengers: Infinity War
+    { id: '1', actor: 'Liam Neeson' },
+    { id: '2', actor: 'Viggo Mortensen' },
+    { id: '3', actor: 'Russell Crowe' },
+    { id: '4', actor: 'Mel Gibson' },
+    { id: '5', actor: 'Leonardo DiCaprio' },
+    { id: '6', actor: 'Daniel Radcliffe' },
+    { id: '7', actor: 'Tobin Bell' },
+    { id: '8', actor: 'Michael J. Fox' },
+    { id: '9', actor: 'Ewan McGregor' },
+    { id: '10', actor: 'Hugh Jackman' },
+    { id: '11', actor: 'Emma Watson' },
+    { id: '12', actor: 'Orlando Bloom' },
+    { id: '13', actor: 'Johnny Depp' },
+    { id: '14', actor: 'Mark Hamill' },
+    { id: '15', actor: 'Scarlett Johansson' }
   ];
   
